5e753dd9016aa296c7b765e512c2eed8143a7462
Nataliya Maksymiv <115217100+NataliyaMaksymiv@users.noreply.github.com>
2023-11-20 19:41:06 +0200

    Update app3.js

--- app3.js
+++ app3.js
@@ -91,7 +91,7 @@ function lastElem(arr, num = 1) {
 function titleCase(str) {
     let splitStr = str.split(" ");
     for (let i = 0; i < splitStr.length; i++) {
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].slice(1);
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
     }
     return splitStr.join(" ");    
 }
